c9bac7a83c23e3c607b00db7b926ea798c903ede
ManuJS <emanuelle.menali@hotmail.com>
2016-08-09 18:48:15 -0300

    first commit

---
+++ app/src/androidTest/java/em/android/lista_livro_api/ApplicationTest.java
@@ -0,0 +1,13 @@
+package em.android.lista_livro_api;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
